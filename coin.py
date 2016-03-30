d7db5ad73b6e2bb7f4eec62994c0fa65dac74507
Davide Gessa <gessadavide@gmail.com>
2016-03-30 10:37:05 +0200

    coin.py balance update

--- coin.py
+++ coin.py
@@ -17,9 +17,20 @@ class Coin:
 	def getBalance (self, address):
 		return self.state [address]
 
+		
 	@update (0x01)
 	def send (self, value, to):
+		fr = self.message.address
+
+		if not fr in self.state:
+			return None
+
+		# Not enough funds
+		if self.state[fr] < value:
+			return None
+
 		if not to in self.state:
 			self.state [to] = 0.0
 
+		self.state [fr] -= value
 		self.state [to] += value
